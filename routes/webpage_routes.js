bc17fc212a8734c480b324be0b8d64c5c950774d
yongbin999 <yongbinliang@umass.edu>
2016-04-02 20:43:22 -0400

    reformat to hader footer partials

--- routes/webpage_routes.js
+++ routes/webpage_routes.js
@@ -7,7 +7,11 @@ router.get('/', function(req, res) {
       			res.render('home_page');
 });
 router.get('/test', function(req, res) {
-            res.render('index');
+            res.render('view1');
+});
+
+router.get('/test2', function(req, res) {
+            res.render('view2');
 });
 
 
